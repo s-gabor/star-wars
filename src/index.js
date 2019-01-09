3f32288790c782eee58a3025077a9a005f07d9c6
s-gabor <ing.gabor@yahoo.com>
2019-01-09 12:05:10 +0200

    load all data, format code to ES6

--- src/index.js
+++ src/index.js
@@ -1,69 +1,47 @@
-// const userInput = document.getElementById('searches');
-// userInput.onkeyup = event => console.log(event.target.value);
-// function updateOutputTitle(value) {
-// 	document.getElementsByTagName('h3')[0].textContent = value.toUpperCase();
-// }
-
 const baseURL = 'https://swapi.co/api/';
-let ulResult = document.getElementById('result');
-let selectedFilter;
+const ulResult = document.getElementById('ul-result');
 
-function fetchAndDisplayResult(url) {
+
+const addChildToParent = (child, parent, data) => {
+	let childElement = document.createElement(child);
+	let textNode = document.createTextNode(data);
+	childElement.appendChild(textNode);
+	parent.appendChild(childElement);
+}
+
+
+const fetchAndDisplayData = (selection) => {
+	let url = baseURL + selection;
 	fetch(url)
-			.then(result => result.json())
-			.then(result => {
-						let resultList = result.results;
-						for (i = 0; i < resultList.length; i++) {
-							document.getElementsByTagName('li')[i].textContent = resultList[i].name;
-						}
-					}
-				)
+			.then(results => results.json())
+			.then(resultsParsed => {
+				// load first 10 items
+				let first10items = resultsParsed.results;
+				for (let item of first10items) {
+					addChildToParent('li', ulResult, item.name);
+				}
+				// load the remaining items
+				for (i = 10; i < resultsParsed.count; i++) {
+					let url = baseURL + selection + '/' + i;
+					fetch(url)
+						.then(result => result.json())
+						.then(item => {
+								if (item.detail !== 'Not found') {
+									addChildToParent('li', ulResult, item.name);
+								}
+							})
+				}
+			})
 }
-
-
-window.alert = function(title, message){
-    var myElementToShow = document.getElementById("my-alert");
-    myElementToShow.innerHTML = title + "</br>" + message; 
-}
-
-// document.getElementById('searches').addEventListener('keyup', function(event) {
-// 	if (event.keyCode === 13) {
-// 		selectedFilter = event.target.value;
-// 		console.log('searches: ', selectedFilter);
-// 		let url = baseURL + selectedFilter;
-// 		fetchAndDisplayResult(url)
-// 	}
-// })
 
 
 document.getElementById('filters').addEventListener('change', (event) => {
-	selectedFilter = event.target.value;
-	console.log('filters: ', selectedFilter);
-	let url = baseURL + selectedFilter;
-	fetchAndDisplayResult(url);
+	let selection = event.target.value;
+	document.getElementById('ul-result').innerHTML = '';
+	fetchAndDisplayData(selection);
 });
 
 
 
 
 
-
-ulResult.addEventListener('click', (event) => {
-							// console.log(event.target);
-							// console.log('click: ', selectedFilter);
-							let url = baseURL + selectedFilter + '/' + event.target.value;
-							// console.log(url);
-							fetch(url)
-								.then(result => result.json())
-								.then(result2 => {
-									let arrItem = Object.entries(result2);
-									let output = '';
-									for (let i = 0; i < 8; i++) {
-										// console.log(entry);
-										output += (arrItem[i][0] + ': ' + arrItem[i][1]) + '<br>';
-									}
-									alert(result2.name + '<br>', output);
-								})
-								.catch(err => alert('No additional information!'))
-							}
-						)
