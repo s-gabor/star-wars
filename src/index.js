ae9c1e96839c7480d34dfd70ee8c4daf5a946ba6
s-gabor <ing.gabor@yahoo.com>
2019-01-09 14:08:47 +0200

    add functionality to search box

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const baseURL = 'https://swapi.co/api/';
 const ulResult = document.getElementById('ul-result');
 const divSearch = document.getElementById("searchField");
+let resultsArray;
 
 
 const addChildToParent = (child, parent, data) => {
@@ -12,6 +13,7 @@ const addChildToParent = (child, parent, data) => {
 
 
 const fetchAndDisplayData = (selection) => {
+	resultsArray = [];
 	let url = baseURL + selection;
 	fetch(url)
 			.then(results => results.json())
@@ -22,6 +24,7 @@ const fetchAndDisplayData = (selection) => {
 					let data;
 					selection.toLowerCase() === 'films' ? data = item.title : data = item.name;
 					addChildToParent('li', ulResult, data);
+					resultsArray.push(data);
 				}
 				// load the remaining items (if more than 10)
 				for (i = 10; i < resultsParsed.count; i++) {
@@ -31,6 +34,7 @@ const fetchAndDisplayData = (selection) => {
 						.then(item => {
 							if (item.detail !== 'Not found') {
 								addChildToParent('li', ulResult, item.name);
+								resultsArray.push(item.name);
 							}
 						})
 				}
@@ -39,16 +43,61 @@ const fetchAndDisplayData = (selection) => {
 
 
 divSearch.style.display = "none";
-
 document.getElementById('filters').addEventListener('change', (event) => {
-	let selection = event.target.value;
-	document.querySelector('body > h3').innerHTML = '';
 	divSearch.style.display = "flex";
-	document.querySelector('#searchField > input').setAttribute('placeholder', 'search for ' + selection);
-
-	document.getElementById('ul-result').innerHTML = '';
+	ulResult.innerHTML = '';
+	document.querySelector('body > h3').innerHTML = '';
+	let selection = event.target.value;
+	document.querySelector('#searchField > input')
+		.setAttribute('placeholder', 'search for ' + selection);
 	fetchAndDisplayData(selection);
 });
+
+
+document.querySelector('#searchField > input').addEventListener('keyup', (event) => {
+	let resultsArrayFiltered = [];
+	if (event.target.value.length > 0) {
+		// display filtered array
+		ulResult.innerHTML = '';
+		for (let item of resultsArray) {
+			if (item.toLowerCase().includes(event.target.value.toLowerCase())) {
+				resultsArrayFiltered.push(item);
+				}	
+		}
+		for (let item of resultsArrayFiltered) {
+			addChildToParent('li', ulResult, item);
+		}
+	} else {
+		// display full array
+		ulResult.innerHTML = '';
+		for (let item of resultsArray) {
+			addChildToParent('li', ulResult, item);
+		}
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
